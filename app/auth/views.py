79d9becba23db4cd3991fed8b7bd5ac6ae8de13e
chenglinguang <clg19881220@163.com>
2016-07-30 23:18:41 +0800

    user registration and user permissions

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,12 @@
 from flask import render_template,redirect,request,url_for,flash
-from flask_login import login_user,logout_user,login_required
+from flask_login import login_user,logout_user,login_required,current_user
 from . import auth
 from ..models import User
-from .forms import LoginForm
+from .forms import LoginForm,RegistrationForm,ChangePasswordForm
+from .. import db
+from .decorators import admin_required, permission_required
+from .models import Permission
+
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
@@ -11,7 +15,8 @@ def login():
         user=User.query.filter_by(email=form.email.data).first()
         if user is not None and user.verify_password(form.password.data):
             #login_user把用户标记为已经登陆
-            login_user(user,form.remember_me.data)
+            #login_user(user,form.remember_me.data)
+            login_user(user,False) #不让下次登录记住用户
             return redirect(request.args.get('next') or url_for('main.index'))
         flash('Invalid username or password.')
     return render_template('auth/login.html',form=form)
@@ -23,5 +28,49 @@ def logout():
     logout_user()
     flash('您已经退出登陆.')
     return redirect(url_for('main.index'))
+
+@auth.route('/register',methods=['GET','POST'])
+def register():
+    form=RegistrationForm()
+    if form.validate_on_submit():
+        user=User(email=form.email.data,username=form.username.data,password=form.password.data)
+        db.session.add(user)
+        flash('You can now login.')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
+
+@auth.route('/change-password',methods=['GET','POST'])
+@login_required
+def change_password():
+    form = ChangePasswordForm()
+    if form.validate_on_submit():
+        if current_user.verify_password(form.old_password.data):
+            current_user.password = form.password.data
+            db.session.add(current_user)
+            flash('Your password has been updated.')
+            return redirect(url_for('main.index'))
+        else:
+            flash('Invalid password.')
+    return render_template('auth/change_password.html',form=form)
+
+@main.route('/admin')
+@login_required
+@admin_required
+def for_admins_only():
+    return "For administrators!"
+
+
+@main.route('/moderator')
+@login_required
+@permission_required(Permission.MODERATE_COMMENTS)
+def for_moderators_only():
+    return "For comment moderators!"
+
+
+
+
+
+
+
 
 
--- app/models.py
+++ app/models.py
@@ -1,14 +1,48 @@
 from flask_sqlalchemy import SQLAlchemy
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin
+from flask_login import UserMixin,AnonymousUserMixin
 from . import login_manager
+
+
+
+class Permission:
+    FOLLOW=0x01
+    COMMENT = 0x02
+    WRITE_ARTICLES = 0x04
+    MODERATE_COMMENTS = 0x08
+    ADMINISTER = 0x80
 
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
+    #只有一个角色的default 字段要设为True，其他都设为False。用户注册时，其角色会被设为默认角色。
+    default = db.Column(db.Boolean, default=False, index=True)
+    permissions = db.Column(db.Integer)
     users=db.relationship('User',backref='role')
+    
+    @staticmethod
+    def insert_roles():
+        roles={
+            'User': (Permission.FOLLOW |
+                     Permission.COMMENT |
+                     Permission.WRITE_ARTICLES, True),
+            'Moderator': (Permission.FOLLOW |
+                          Permission.COMMENT |
+                          Permission.WRITE_ARTICLES |
+                          Permission.MODERATE_COMMENTS, False),
+            'Administrator': (0xff, False)
+        }
+        for r in roles:
+            role = Role.query.filter_by(name=r).first()
+            if role is None:
+                role = Role(name=r)
+            role.permissions = roles[r][0]
+            role.default = roles[r][1]
+            db.session.add(role)
+        db.session.commit()
+
     def __repr__(self):
         return '<Role %r>' % self.name
 
@@ -20,6 +54,14 @@ class User(UserMixin,db.Model):
     password_hash=db.Column(db.String(128))
     role_id=db.Column(db.Integer,db.ForeignKey('roles.id'))
 
+    def __init__(self,**kwargs):
+        super(User, self).__init__(**kwargs)
+        if self.role is None:
+            if self.email == current_app.config['FLASKY_ADMIN']:
+                self.role = Role.query.filter_by(permissions=0xff).first()
+            if self.role is None:
+                self.role = Role.query.filter_by(default=True).first()
+
     @property
     def password(self):
         raise AttributeError('password is not a readable attribute')
@@ -30,9 +72,22 @@ class User(UserMixin,db.Model):
     
     def verify_password(self,password):
         return check_password_hash(self.password_hash,password)
+    def can(self,permissions):
+        return self.role is not None and (self.role.permissions & permissions) == permissions
+    def is_administrator(self):
+        return self.can(Permission.ADMINISTER)
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+class AnonymousUser(AnonymousUserMixin):
+    def can(self, permissions):
+        return False
+
+    def is_administrator(self):
+        return False
+
+
 
 @login_manager.user_loader
 def load_user(user_id):
