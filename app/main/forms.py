88b050092376fc4f4ea5ffd689b3b2612f7e18a2
chenglinguang <clg19881220@163.com>
2016-08-04 15:54:58 +0800

    finshed basic functions

--- app/main/forms.py
+++ app/main/forms.py
@@ -46,5 +46,10 @@ class PostForm(Form):
     body=PageDownField("What's on your mind?", validators=[Required()])
     submit = SubmitField('Submit')
 
+class CommentForm(Form):
+    body=StringField('',validators=[Required()])
+    submit=SubmitField('Submit')
+
+
 
 
